1ebc0f5cc6d6c4a01d85e17712e526fed0f9a61a
Darren Brown <darrenb@iqmetrix.com>
2014-06-13 09:31:53 -0600

    Created empty dungeon generation

--- LearningMoreFPound/Program.fs
+++ LearningMoreFPound/Program.fs
@@ -3,6 +3,7 @@
 
 
 open System
+open System.IO
 open System.Collections
 open System.Text
 open System.Threading
@@ -13,6 +14,26 @@ open Microsoft.FSharp.Reflection
 [<EntryPoint>]
 let main argv = 
 
+    printf "Enter dungeon size: "
+    let dungeonSize = int32 (Console.ReadLine())
+
+    let buildDungeon size = 
+        let emptyDungeon = Array2D.create (size + 1) (size + 1) '.'
+
+        let wallDungeon size (dungeon:char[,]) =
+            let adjustedSize = size - 1
+            for i = 0 to adjustedSize do
+                dungeon.SetValue( '#', [|0; i;|])
+                dungeon.SetValue( '#', [|adjustedSize; i;|])
+                dungeon.SetValue( '#', [|i; 0;|])
+                dungeon.SetValue( '#', [|i; adjustedSize;|])
+            dungeon
+
+        let walledDungeon = wallDungeon (size + 1) emptyDungeon
+        printfn "%A" walledDungeon
+        printfn "wakka"
+    
+    buildDungeon dungeonSize
 
     printf "\nPress any key to continue..."
     Console.ReadKey(true) |> ignore
