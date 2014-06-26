036a05d3bcb9044aa4a5eff150de9f82a4f70905
Darren Brown <darrenb@iqmetrix.com>
2014-06-26 09:28:03 -0600

    intial branch commit added unfolding method

--- LearningMoreFPound/Program.fs
+++ LearningMoreFPound/Program.fs
@@ -12,6 +12,18 @@ open Microsoft.FSharp.Reflection
 
 [<EntryPoint>]
 let main argv = 
+    let fizzy n =
+        match n with
+        | x when ((x % 3) = 0 && (x % 5 = 0)) -> Some("FizBuz", n + 1)
+        | x when ((x % 3) = 0) -> Some("Fizz", n + 1)
+        | x when ((x % 5 = 0)) -> Some("Buzz", n + 1)
+        | x when x > 100 -> None
+        | _ -> Some ( n.ToString(), n + 1)
+
+    let doubleIt n =
+        n |> Seq.unfold(fun x -> fizzy x)        
+
+    doubleIt -100 |> Seq.iter (fun x -> printf "%s \t" x)
 
 
     printf "\nPress any key to continue..."
