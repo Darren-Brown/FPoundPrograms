fc5ccf55a585272ec1186a0b7a833aebbc54088f
Darren Brown <darrenb@iqmetrix.com>
2014-07-25 13:52:32 -0600

    fixed format to conform to maze solver input

--- LearningMoreFPound/Program.fs
+++ LearningMoreFPound/Program.fs
@@ -138,6 +138,8 @@ let main argv =
     //printfn "%A" tempMaze
     let printMaze (maze:char[,]) =
         use wr = new StreamWriter ("maze.txt", false)
+        let sizeString = (xSize + 1).ToString() + " " + (ySize + 1).ToString()
+        wr.WriteLine (sizeString)
         for i = 0 to xSize + 1 do
             for j = 0 to ySize + 1 do
                 wr.Write(maze.[i,j])
