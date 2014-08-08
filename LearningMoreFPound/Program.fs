3bd46f98cac3440e70c113f3c03150c4eda471a5
Darren Brown <darrenb@iqmetrix.com>
2014-08-08 09:36:47 -0600

    adjusted to make background images added support to perform more iterations before dumping the image

--- LearningMoreFPound/Program.fs
+++ LearningMoreFPound/Program.fs
@@ -36,37 +36,45 @@ let main argv =
         cprintf c fmt
         printfn ""
 
-    printf "Please enter direction string size:"
-    let inputSize = Math.Min(Int32.Parse(Console.ReadLine()), 765)
+//    printf "Please enter direction string size:"
+    //let inputSize = Math.Min(Int32.Parse(Console.ReadLine()), 111765)
+    let inputSize = 4080
     let generateDirection inputVal=
         match inputVal with
         | 0 -> 'L'
         | 1 -> 'R'
         | _ -> 'E'         
 
-    printf "Please enter area size:"
-    let areaSize = Int32.Parse(Console.ReadLine())
+//    printf "Please enter area size:"
+    //let areaSize = Int32.Parse(Console.ReadLine())
+
+    // for background
+    let xSize = 1920
+    let ySize = 1080
 
     let colorIncrement = Math.Max(1, 255 / inputSize)
-    //let colorArray = Array.init (inputSize) (fun index -> Color.FromArgb(Math.Min(255, (index * colorIncrement)), 255, 255))
+    //let colorArray = Array.init (inputSize) (fun index -> Color.FromArgb(index % 255, (index / 255) % 255, (index / 65025) % 255))
+    let colorFunction index = Color.FromArgb((index / 16) % 255, ( index / 4) % 255, ( index % 255))
     //let colorArray = Array.init (inputSize) (fun index -> Color.FromArgb(Math.Min(255, (index * colorIncrement)), (Math.Min(255, Math.Max(0, (index - 100) * colorIncrement))), Math.Min(50, (colorIncrement * index)/6)))
-    let colorArray = Array.init (inputSize) (fun index -> Color.FromArgb(Math.Min(255, (index * colorIncrement)), (Math.Min(255, Math.Max(0, (index - (inputSize/3)) * colorIncrement))), (Math.Min(255, Math.Max(0, (index - (2*inputSize/3)) * colorIncrement)))))
+    //let colorArray = Array.init (inputSize) (fun index -> Color.FromArgb( (Math.Min(255, Math.Max(0, (index - (2*inputSize/3)) * colorIncrement))), (Math.Min(255, Math.Max(0, (index - (inputSize/2)) * colorIncrement))), Math.Min(255, (index * colorIncrement))))
+    //let colorArray = Array.init (inputSize) (fun index -> Color.FromArgb( (Math.Min(200, Math.Max(0, (index - (2*inputSize/3)) * colorIncrement))), (Math.Min(200, Math.Max(0, (index - (inputSize/2)) * colorIncrement))), Math.Min(200, (index * colorIncrement))))
+    
     //    let colorArray = Array.init (inputSize) (fun index -> Color.FromArgb((Math.Min(255, index * colorIncrement)), (Math.Min(255, Math.Max( 0, (index - 510) * colorIncrement))), (Math.Min(255, Math.Max( 0, (index - 255) * colorIncrement)))))
 
-    let initialPosition = [|(areaSize/2); (areaSize/2)|]
-
+//    let initialPosition = [|(xSize/2); (ySize/2)|]
+    let initialPosition = [|0;0|]
     let printBoardImage (maze:int[,]) =
-        let test = new Bitmap(areaSize, areaSize)
+        let test = new Bitmap(xSize, ySize)
 //        let testCC = (enum<ConsoleColor>(board.[0,0]))
 //        let test2 = testCC.ToString()
 //        let testC = Color.FromName test2
 //        //use wr = new StreamWriter ("maze.txt", false)
 //        //let sizeString = (areaSize + 2).ToString() + " " + (ySize + 2).ToString()
 //        //wr.WriteLine (sizeString)
-        for i = 0 to (areaSize - 1) do
-            for j = 0 to (areaSize - 1) do
+        for i = 0 to (xSize - 1) do
+            for j = 0 to (ySize - 1) do
 //                test.SetPixel(i, j, (Color.FromName ((enum<ConsoleColor>(maze.[i,j])).ToString())))
-                test.SetPixel(i, j, colorArray.[maze.[i,j]])
+                test.SetPixel(i, j, (colorFunction maze.[i,j]))
         
         test.Save("daBoard.bmp", System.Drawing.Imaging.ImageFormat.Bmp)
 
@@ -80,20 +88,17 @@ let main argv =
         match direction with
         //north
         | 0 ->  let newY = curPosition.[1] - 1
-                [| curPosition.[0]; Math.Abs(((newY) + areaSize) % areaSize);|]
+                [| curPosition.[0]; Math.Abs(((newY) + ySize) % ySize);|]
         //east
         | 1 ->  let newX = curPosition.[0] + 1
-                [| Math.Abs(((newX) + areaSize) % areaSize); curPosition.[1];|]
+                [| Math.Abs(((newX) + xSize) % xSize); curPosition.[1];|]
         //south
         | 2 ->  let newY = curPosition.[1] + 1
-                [| curPosition.[0]; Math.Abs(((newY) + areaSize) % areaSize);|]     
+                [| curPosition.[0]; Math.Abs(((newY) + ySize) % ySize);|]     
         // west
         | 3 ->  let newX = curPosition.[0] - 1
-                [| Math.Abs(((newX) + areaSize) % areaSize); curPosition.[1];|]
+                [| Math.Abs(((newX) + xSize) % xSize); curPosition.[1];|]
         | _ -> curPosition
-    
-
-    
 
     let rec mainLoop patternSize printAtInterval terminateAtInterval=
 
@@ -102,29 +107,26 @@ let main argv =
             let newPosition = getNewAntPosition antPos newDirection
             board.SetValue ((board.[antPos.[0], antPos.[1]] + 1) % (directionArray.Length), antPos)
         
-            if printCountdown < 1 then
+            if printCountdown < 1L then
                 printfn "began printing board at %s" (DateTime.Now.ToString())
                 printBoardImage board
                 printfn "new board generated at %s" (DateTime.Now.ToString())
-                Thread.Sleep(1500)
+                //Thread.Sleep(1500)
                 if terminateCountdown < 1 then
                     printfn "finished pattern %A" directionArray
                     Thread.Sleep(5000)
                 else
                     runAnt directionArray newPosition newDirection board printAtInterval (terminateCountdown - 1)
             else
-                runAnt directionArray newPosition newDirection board (printCountdown - 1) terminateCountdown          
+                runAnt directionArray newPosition newDirection board (printCountdown - 1L) terminateCountdown          
 
         let inputDirectionArray = Array.init patternSize (fun _ -> rand.Next(0, 2) |> generateDirection)
-        let board = Array2D.init areaSize areaSize (fun _ _ -> 0)
+        let board = Array2D.init xSize ySize (fun _ _ -> 0)
         runAnt inputDirectionArray initialPosition 0 board printAtInterval terminateAtInterval
         mainLoop patternSize printAtInterval terminateAtInterval
 
-    
-    
-
-   // printMazeImage board
-    mainLoop inputSize 1000000 (60)
+    // printMazeImage board
+    mainLoop inputSize 500000000L (5)
     printf "\nPress any key to continue..."
     Console.ReadKey(true) |> ignore
     0 // return an integer exit code
