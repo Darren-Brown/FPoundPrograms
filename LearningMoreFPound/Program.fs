db52c0c1de745be5ca1b6cbf427d7276242a6a3e
Darren Brown <darrenb@iqmetrix.com>
2014-08-18 16:54:04 -0600

    swapped chaining words with chaining letters; added variable key size; added loop to display new strings until the 'X' key is pressed;

--- LearningMoreFPound/Program.fs
+++ LearningMoreFPound/Program.fs
@@ -17,73 +17,74 @@ let main argv =
     
     let getInputFromFile (filepath:string)  =
         use sr = new StreamReader (filepath)
-        let rec buildInputList (tempList:List<string>) (sr:StreamReader) =
+        let rec buildInputList (tempList:List<char>) (sr:StreamReader) =
             if not sr.EndOfStream then
-                let temp = sr.ReadLine().Split([|' '|]) |> Array.toList
+                let temp = sr.ReadLine().ToCharArray() |> Array.toList
                 buildInputList (List.append tempList temp) sr
             else
                 tempList
 
         buildInputList List.Empty sr
 
-    let input = (getInputFromFile "input.txt") |> Seq.toList 
+    let input = (getInputFromFile "input.txt") 
 
-    let rec learn prevWord (inputList:List<string>) (dict:Map<string, List<string>>) =
-        let curWord = inputList.Head
-        let isStop = curWord.EndsWith(".")
-        let cleanWord = match isStop with
-                        | true -> curWord.Substring(0, curWord.Length - 1)
-                        | false -> curWord
-        let curKey = prevWord + " " + cleanWord
-
-        let nextValue = match inputList.Tail.IsEmpty with
-                        | true -> ""
-                        | false ->  if inputList.Tail.Head.EndsWith(".") then
-                                        inputList.Tail.Head.Substring(0, inputList.Tail.Head.Length - 1)
-                                    else
-                                        inputList.Tail.Head
-
-        let newDict = match dict.ContainsKey curKey with
-                        | true ->   let entry = dict.Item(curKey)
-                                    let tempDict = dict.Remove(curKey)
-                                    let newPossibilitesList = match isStop with 
-                                                                | true -> "." :: entry
-                                                                | false -> nextValue :: entry
-                                    tempDict.Add(curKey, newPossibilitesList)
-                        | false ->  match isStop with
-                                    | true -> dict.Add(curKey, "." :: List.Empty)
-                                    | false -> dict.Add(curKey, nextValue::List.Empty)
-        if inputList.Tail.Tail.IsEmpty then
-            newDict
+    let rec learn (key:string) (inputList:List<char>) (dict:Map<string, List<char>>) keyLength =
+        let curChar = inputList.Head
+        if key.Length < keyLength then
+            learn (key + curChar.ToString()) inputList.Tail dict keyLength
         else
-            if isStop then
-                learn nextValue inputList.Tail.Tail newDict
+            let newDict = match dict.ContainsKey key with
+                            | true ->   let entry = dict.Item(key)
+                                        let tempDict = dict.Remove(key)
+                                        let newPossibilitesList = curChar :: entry
+                                        tempDict.Add(key, newPossibilitesList)
+                            | false ->  dict.Add(key, curChar::List.Empty)
+            if inputList.Tail.IsEmpty then
+                newDict
             else
-                learn cleanWord inputList.Tail newDict
+                learn (key.Substring(1, key.Length - 1) + curChar.ToString()) inputList.Tail newDict keyLength
              
-    let test = learn input.Head input.Tail Map.empty
+    let test = learn "" input Map.empty 3
 
-    let generateString (dict:Map<string, List<string>>) (rnd:Random) =       
-        let randomStart = rnd.Next(1, dict.Count)
-        let randomStartList = (Map.toArray dict).[randomStart]
-        let randomStartKey = (fst randomStartList)
+    let rec generateString (dict:Map<string, List<char>>) (rnd:Random) =      
 
-        let rec printString currentKey (dict:Map<string, List<string>>) (rnd:Random) =
-            let potentials = dict.Item(currentKey)
-            let randomIndex = rnd.Next(0, potentials.Length)
-            let randomString = potentials.[randomIndex]
-            let newKey = (currentKey.Split([|' '|]).[1]) + " " + randomString
-                    
-            if newKey.EndsWith(".") then
-                printfn "\b%s" randomString
+        let rec getKeyWithLeadingCapital (dict:Map<string, List<char>>) (rnd:Random) =
+            let randomIndex = rnd.Next(1, dict.Count)
+            let randomDictEntry = (Map.toArray dict).[randomIndex]
+            let randomDictKey = fst randomDictEntry
+
+            if Char.IsUpper(randomDictKey, 0) && not (randomDictKey.Contains(".") )then
+                randomDictKey
             else
-                printf "%s " (randomString)
-                printString newKey dict rnd
+                getKeyWithLeadingCapital dict rnd
 
-        printString randomStartKey dict rnd
+        let rec buildString (subString:List<char>) (key:string) (dict:Map<string, List<char>>) (rnd:Random) = 
+            let curCharList = dict.Item key
+            let randomCharIndex = rnd.Next(0, (curCharList.Length))
+            let randomChar = curCharList.[randomCharIndex]
 
-    for i = 0 to 10 do
-        generateString test rand
+            if randomChar = '.' then
+                (List.rev (randomChar :: subString)) |> List.toArray |> (fun s -> System.String s)
+            else
+                let newSubString = randomChar :: subString
+                let newKey = (key.Substring(1, key.Length - 1) + randomChar.ToString())
+                buildString newSubString newKey dict rnd
+
+        let randomDictKey = getKeyWithLeadingCapital dict rnd
+        buildString (randomDictKey.ToCharArray() |> Array.toList |> List.rev) randomDictKey dict rnd
+
+//    for i = 0 to 10 do
+//        let testString = generateString List.Empty test rand
+//        printfn "%s" testString
+
+    let rec infiniGen () =
+        Console.Clear()
+        printfn "%s" (generateString test rand)
+        let input = Console.ReadKey(true).KeyChar
+        if not (input.Equals 'x') then            
+            infiniGen ()
+
+    infiniGen()
     printf "\nPress any key to continue..."
     Console.ReadKey(true) |> ignore
     0 // return an integer exit code
